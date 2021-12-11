1da79557661b5b5a20d67d83cc7b6528ccd8a026
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-12-11 10:09:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by Matteo on 11/12/2021.
+//
+
+#include "lib.h"
+#include <string.h>
+bool res (char lista_nomi[10][20],char nome[20],int &i){
+    for (i=0;i<10;i++){
+        if((strcmp(lista_nomi[i],nome))==0){
+            return true;
+        }
+    }
+    return false;
+}
 
