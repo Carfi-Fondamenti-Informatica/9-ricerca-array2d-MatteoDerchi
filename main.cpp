5c81e78276a034d969a4ae7cbf055302dff87c7a
MatteoDerchi <91695814+MatteoDerchi@users.noreply.github.com>
2021-12-11 10:09:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lista_nomi [10][20];
+    char nome [20];
+    int a=0;
+    for(int i=0;i<10;i++){
+        cin>>lista_nomi[i];
+    }
+    cin>>nome;
+    if (res(lista_nomi,nome,a)){
+        cout<<a;
+    }else{
+        cout<<"non presente";
+    }
+    return 0;
 }
+
